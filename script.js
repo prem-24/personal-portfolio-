005e96551feeb698c47faecc8b10b95a9f2fa85e
prem-24 <prem24052000@gmail.com>
2024-05-29 09:54:47 +0530

    c

--- script.js
+++ script.js
@@ -12,4 +12,31 @@ document.addEventListener("DOMContentLoaded", function() {
       viewBtn.textContent = "View More"; // Change button text back to "View More"
     }
   });
+
+  document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent the form from submitting normally
+
+    sendEmail(); // Call the sendEmail function
+    resetForm(); // Call the resetForm function
+  });
 });
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "prem2452000@gmail.com",
+    Password: "612B8B9725C34BEB5D367CF20416F427D951",
+    To: 'prem24052000@gmail.com',
+    From: document.querySelector('#email').value,
+    Subject: "This is the subject",
+    Body: "Name: " + document.querySelector('#name').value + "<br>Email: " + document.querySelector('#email').value + "<br>Message: " + document.querySelector('#message').value
+  }).then(
+    message => alert(message)
+  );
+}
+
+function resetForm() {
+  document.querySelector('#name').value = '';
+  document.querySelector('#email').value = '';
+  document.querySelector('#message').value = '';
+}
